9b92fb27e48be416c1831b2b01979d1ece5e1d8a
muhammetali <muhammetalidere@gmail.com>
2021-03-30 16:17:00 +0300

    Update contentController.js

--- app/controllers/contentController.js
+++ app/controllers/contentController.js
@@ -2,14 +2,7 @@
  * Created by muhammet.dere on 26.09.2016.
  */
 siteApp.controller('contentController', ['$scope', '$location', '$routeParams', '$http', function($scope, $location, $routeParams, $http){
-    //console.log("contentController", $routeParams.contentUrl);
-
-
     $scope.url = $routeParams.contentUrl ? $routeParams.contentUrl : $location.path().substring(1);
-
-    // url deki '/' lar '.' ile yer değiştiriliyor.
-    console.log("url", $scope.url);
-
 
     $http.get('/helper/getDummyContent.php?url=' + $scope.url)
         .then(function(response){
